adc2ce3afca9bf72c92d1db615a0f38c96a1c0af
lCodecrafter <lcodecrafter@gmail.com>
2025-03-13 13:28:44 +0100

    Add testing providers for RQ and Redux

--- src/tests/tools.tsx
+++ src/tests/tools.tsx
@@ -1,11 +1,51 @@
-import { render, screen, fireEvent, within, waitFor, act } from '@testing-library/react';
+import {
+  render as rtlRender,
+  renderHook as rtlRenderHook,
+  screen,
+  fireEvent,
+  within,
+  waitFor,
+  act,
+} from '@testing-library/react';
 import type { ReactNode } from 'react';
 import { MemoryRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Provider } from 'react-redux';
+import { store } from '@/app/store';
 
-function renderWithRouter(ui: ReactNode) {
-  return {
-    ...render(<MemoryRouter>{ui}</MemoryRouter>),
-  };
+// eslint-disable-next-line react-refresh/only-export-components
+function Providers({ children }: { children: ReactNode }) {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false, // deactivate retries
+        staleTime: 0,
+        gcTime: 0, // deactivate garbage collection
+
+        // deactivate refetch
+        refetchOnMount: false,
+        refetchOnWindowFocus: false,
+        refetchOnReconnect: false,
+      },
+    },
+  });
+
+  queryClient.clear(); //reset queryClient between tests
+  return (
+    <MemoryRouter>
+      <Provider store={store}>
+        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      </Provider>
+    </MemoryRouter>
+  );
 }
 
-export { renderWithRouter as render, screen, fireEvent, within, waitFor, act };
+function render(ui: ReactNode) {
+  return rtlRender(<Providers>{ui}</Providers>);
+}
+
+function renderHookWithProviders<T>(callback: () => T) {
+  return rtlRenderHook(callback, { wrapper: Providers });
+}
+
+export { render, renderHookWithProviders as renderHook, screen, fireEvent, within, waitFor, act };
